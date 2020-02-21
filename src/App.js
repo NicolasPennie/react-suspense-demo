2acba9547492747c91ff566fdfc26f92b11b5cbc
Nicolas Pennie <nicolas.pennie@gmail.com>
2020-02-21 12:16:04 -0500

    Create homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Suspense App</h1>
       </header>
+      <form className="App-login-form">
+        <label>
+          Email
+          <input type="email" name="email"/>
+        </label>
+        <label>
+          Password
+          <input type="password" name="password"/>
+        </label>
+      </form>
+      <footer class="create-account-footer">
+        <span>
+          Don't have an account? &nbsp;
+        </span>
+        <a href="./create" class="create-account-link">Create one now!</a>
+      </footer>
     </div>
   );
 }
